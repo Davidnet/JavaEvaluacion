ac273d41745ab0a3effa30e8008963f9d09716c0
dgromov <david.cardozo@me.com>
2017-04-25 05:50:15 -0500

    hola

--- PanelInfoJugador2.java
+++ PanelInfoJugador2.java
@@ -2,4 +2,25 @@ public PanelInfoJugador(InterfazLigaBolos interfazLigaBolos)
 {
     TitledBorder border = BorderFactory.createTitledBorder("Información");
     setBorder( border );
+    setLayout(new GridLayout(2,1);
+    ImageIcon icon = new IMageIcon(pJugador.darrutamImagen());
+    Jlabel thumb = new JLabel;
+    add(thumb);
+	panelInfo.setLayout(new GridLayout(5,2);
+	txtNOmbreDelJugador = new JTextField(pJUgador.darNombre())
+	txtPArtidasJugadas = new JTextFiled(pJugador.darPartidasJUgadas());
+	txtPromedioPuntos = new JTextFiled(pJugador.darPromedioPuntos());
+	txtvaio = new JTextFiled("");
+	add(labNOmbre);
+	add(LabPArtidasJugadas)
+	add(LabPromedio)
+	add( new JLabel vacio);
+	add( txtNombreDelJugaor);
+	add( txtPArtidasJugadas)
+	add( txtPromedioPuntos);
+	add( new JCheckbox profesional)
+	add( btnAgregarPuntaje);
+	
+
+        }
 } 
